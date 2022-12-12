cd0ba79dc6f39876556429a547eaf55108ebc41b
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2022-12-12 09:36:48 -0800

    first coding

---
+++ code/code_js/task2.js
@@ -0,0 +1,22 @@
+// title: swapping variables#
+// language: js #
+// dificulty: * #
+// topic: variables#
+
+// taskdescription: try to swap the value of these variable #
+
+// code:
+let a = 'blue';
+let b = 'yellow';
+// your code
+
+console.log(a); //-> 'yellow'
+console.log(b); //-> 'blue'
+// #
+
+// solution:
+let c = a;
+a = b;
+b = c;
+
+// #
