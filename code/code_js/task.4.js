df6477b82fe20cbde29941ae51ac5324dd4f4f16
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2022-12-12 11:48:19 -0800

    added task4

---
+++ code/code_js/task.4.js
@@ -0,0 +1,36 @@
+// title: Speed check#
+// language: js #
+// dificulty: ** #
+// topic: conditions, functions#
+// taskdescription: Checking speed using the function. Speed limit is 70km/h,
+// when under 70, -> return 'OK'
+// if it is not, giving a point for each +5km/h, the point system starts from 75. (e.g. 88 is 3 points.) 
+// use Math.floor()
+// Until the points reaches 12 points -> return the points
+// when points reaches 12 -> return 'Licence suspended'
+
+
+// code:
+function speedCheck(speed){
+  // your code
+  
+  }
+
+console.log(speedCheck(50));
+console.log(speedCheck(97));
+console.log(speedCheck(200));
+
+// #
+
+// solution:
+function checkSpeed(speed) {
+  let points = 0;
+
+  if (speed < 75)
+    return "Ok";
+    
+  if (speed >= 75) points = Math.floor((speed - 70) / 5);
+  if (points < 12)
+    return "point: " + points;
+  return "Licence suspended";
+}
