3238f85df6b443837269b0aef98ddcc75db65f20
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2022-12-15 17:10:08 -0800

    added task11 js

---
+++ code/code_js/tasks/task10/task.js
@@ -0,0 +1,17 @@
+// title: Star #
+// language: js #
+// dificulty: ** #
+// topic: functions loops #
+
+// taskdescription: show the *(star) and each rows adds one * 
+// e.g. *
+//      **
+//      *** #
+
+// code:
+function showStar(row) {
+  // your code
+}
+
+showStar(4)
+// #
