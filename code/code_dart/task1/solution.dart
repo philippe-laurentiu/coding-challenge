ae11ef9db7fc4be301af75aa1d24fa5aec298e86
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2022-12-27 11:04:50 -0800

    added task18 js and task1 dart

---
+++ code/code_dart/task1/solution.dart
@@ -0,0 +1,12 @@
+import 'dart:math';
+
+
+void main(List<String> arguments) {
+  double circle(num radius) {
+    double area = pi * radius * radius; 
+    return area;
+  }
+  print(circle(2));
+}
+
+
---
+++ code/code_dart/task1/task1.dart
@@ -0,0 +1,16 @@
+// title: circle function #
+// language: dart #
+// dificulty: ** #
+// topic: functions #
+
+// taskdescription: write a funtion which caluclate area of circle and return the result #
+
+// code:
+import 'dart:math';
+
+void main(List<String> arguments) {
+  // your code
+  print(circle(2));
+}
+
+// #
